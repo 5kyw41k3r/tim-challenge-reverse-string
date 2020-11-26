20d6f985ea54c2b76f35a05f80f4430f37b6ef65
5kyw41k3r <67189930+5kyw41k3r@users.noreply.github.com>
2020-11-26 20:40:55 +0530

    Update tim-script.js

--- tim-script.js
+++ tim-script.js
@@ -1,7 +1,10 @@
+// Store the userString form data value in a variable
 var url_string = document.URL;
 var url = new URL(url_string);
 var string = url.searchParams.get("userString");
 
+// Reverse the string
 var reversed = string.split("").reverse().join("");
 
+// output reversed value to input box
 document.getElementById("userString").value = reversed;
